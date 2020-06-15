6924e52673fcb30a577b77fac7efce62b2986ecf
rohan.sharma <rohan.sharma@del1-lhp-n67599.synapse.com>
2020-06-15 22:02:47 +0530

    variable name change

--- src/main/com/intercom/MainIntercom.java
+++ src/main/com/intercom/MainIntercom.java
@@ -3,8 +3,8 @@ package main.com.intercom;
 public class MainIntercom {
 
 	public static void main(String[] args) {
-		DisplayCustomers distanceCalculator = new DisplayCustomers();
-			distanceCalculator.processCustomers();
+		DisplayCustomers displayCustomers = new DisplayCustomers();
+		displayCustomers.processCustomers();
 	}
 
 }
